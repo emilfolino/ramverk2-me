53fbc4f1d8a1fe46806518fda0d9a487ce1a9b7a
Emil Folino <emilfolino@gmail.com>
2018-12-11 15:47:57 +0100

    Changed so it shows an array of questions instead of just text.

--- routes/reports.js
+++ routes/reports.js
@@ -3,11 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/kmom01', function(req, res, next) {
-    const data = {
-        text: "Det här är ett grymt kmom."
-    };
+    const questions = [
+        {
+            question: "Berätta utförligt om din syn på nodejs backend ramverk och berätta vilket ramverk du valde och varför.",
+            answer: "Jag valde expressjs som backend ramverk, då jag har stor tidigare erfarenhet med det ramverket och att det är det mest använda nodejs ramverket."
+        },
+        {
+            question: "Berätta om din katalogstruktur och hur du organiserade din kod, hur tänkte du?",
+            answer: "Jag skapade en katalog med routes, som liknar det som kommer från scaffoldingen med express. Varje route fil exporterar en router."
+        },
+    ];
 
-    res.json(data);
+    res.json({ data: questions });
 });
 
 module.exports = router;
