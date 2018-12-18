72ed3d6b2fdf8a35eef8eaf85644fba366474cac
Emil Folino <emilfolino@gmail.com>
2018-12-18 11:21:32 +0100

    Added logging with morgan

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const index = require('./routes/index');
 const reports = require('./routes/reports');
@@ -8,6 +9,12 @@ const app = express();
 app.use(cors());
 
 const port = 8333;
+
+// don't show the log when it is test
+if (process.env.NODE_ENV !== 'test') {
+    // use morgan to log at command line
+    app.use(morgan('combined')); // 'combined' outputs the Apache style LOGs
+}
 
 app.use('/', index);
 app.use('/reports', reports);
