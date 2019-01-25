1b7102caead6a7c48aef5c6e9529f12422c252aa
Emil Folino <emilfolino@gmail.com>
2019-01-25 11:35:15 +0100

    Fixed eslint validation in package.json

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
     const data = {
         name: "Emil Folino",
         city: "Rödeby",
-        description: "Dansk som nog blir mer och mer svensk. Gillar JavaScript, snabba hemsidor och jordgubbstårta.",
+        description: "Dansk som nog blir mer och mer svensk."
+        + " Gillar JavaScript, snabba hemsidor och jordgubbstårta.",
     };
 
     res.json(data);
--- routes/reports.js
+++ routes/reports.js
@@ -2,15 +2,21 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/kmom01', function(req, res, next) {
+router.get('/kmom01', function(req, res) {
     const questions = [
         {
-            question: "Berätta utförligt om din syn på nodejs backend ramverk och berätta vilket ramverk du valde och varför.",
-            answer: "Jag valde expressjs som backend ramverk, då jag har stor tidigare erfarenhet med det ramverket och att det är det mest använda nodejs ramverket."
+            question: "Berätta utförligt om din syn på nodejs backend " +
+            "ramverk och berätta vilket ramverk du valde och varför.",
+            answer: "Jag valde expressjs som backend ramverk," +
+            " då jag har stor tidigare erfarenhet med det ramverket " +
+            "och att det är det mest använda nodejs ramverket."
         },
         {
-            question: "Berätta om din katalogstruktur och hur du organiserade din kod, hur tänkte du?",
-            answer: "Jag skapade en katalog med routes, som liknar det som kommer från scaffoldingen med express. Varje route fil exporterar en router."
+            question: "Berätta om din katalogstruktur" +
+            " och hur du organiserade din kod, hur tänkte du?",
+            answer: "Jag skapade en katalog med routes," +
+            " som liknar det som kommer från scaffoldingen med express." +
+            " Varje route fil exporterar en router."
         },
     ];
 
